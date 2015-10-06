b559c7107f342620e0d63b1efd1f8d6812ebffd3
Joshua Breault <Josh@Joshuas-MacBook-Pro-2.local>
2015-10-06 15:47:03 -0400

    quick fox

---
+++ quick_fox.rb
@@ -0,0 +1,14 @@
+class Animal 
+
+	def jumped_over(lazy_dog, daisy_log)
+		"Quick fox jumped over #{lazy_dog}, and #{daisy_log}"
+	end
+end
+
+
+
+quick_fox = Animal.new
+lazy_dog = "lazy dog"
+daisy_log = "daisy log"
+quick_fox.jumped_over(lazy_dog, daisy_log)
+
